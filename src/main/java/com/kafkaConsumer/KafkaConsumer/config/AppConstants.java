3e49ed9efa461ab136bea325c989f664aafc4651
kos15 <koustubhmishra91@gmail.com>
2023-12-03 01:01:30 +0530

    Initial commit for Kafka-consumer

---
+++ src/main/java/com/kafkaConsumer/KafkaConsumer/config/AppConstants.java
@@ -0,0 +1,8 @@
+package com.kafkaConsumer.KafkaConsumer.config;
+
+public class AppConstants {
+
+    public static final String LOCATION_UPDATE_TPOIC = "location-update-topic";
+
+    public static final String GROUP_ID="group-1";
+}
---
+++ src/main/java/com/kafkaConsumer/KafkaConsumer/config/KafkaConfig.java
@@ -0,0 +1,13 @@
+package com.kafkaConsumer.KafkaConsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+
+    @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TPOIC, groupId = AppConstants.GROUP_ID)
+    public void updateLocation(String value){
+        System.out.println(value);
+    }
+}
